5211aa4d470bb0afa748d1f89c24386051696e13
mekgurnani <mehak_g@outlook.com>
2024-06-20 17:18:37 +0400

    adding all my app files

--- src/app.py
+++ src/app.py
@@ -4,7 +4,6 @@ import numpy as np
 import os
 
 from collections import defaultdict
-import matplotlib.pyplot as plt
 import ecg_plot
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
